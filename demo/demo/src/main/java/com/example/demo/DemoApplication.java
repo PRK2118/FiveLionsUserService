39cd996e1612fb77132a4454ba764933d09fb28c
PRK2118 <114367128+PRK2118@users.noreply.github.com>
2022-10-16 13:36:37 -0400

    updates

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -21,27 +21,27 @@ public class DemoApplication {
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-    @GetMapping("/user")
-    public String sayHell(@RequestParam(value = "myName", defaultValue = "World") String name) {
 
-        return String.format(mysql_query("jdbc:mysql://localhost:3306/e6156_project","root","dbuserdbuser","SELECT * FROM  e6156_project.users"), name);
+    @GetMapping("/testuser")
+    public String basic(@RequestParam(value = "myName", defaultValue = "World") String name) {
+
+        return String.format(mysql_query("jdbc:mysql://users-e6156.cexqeqvqreq2.us-east-1.rds.amazonaws.com:3306/UserData?autoReconnect=true&useSSL=false","root","dbuserdbuser","SELECT * FROM  UserData.users"), name);
     }
-    @RequestMapping("/user/{someID}")
-    public @ResponseBody String getAttr(@PathVariable(value="someID") String id,
-                                     String someAttr) {
-        return String.format(mysql_query("jdbc:mysql://localhost:3306/e6156_project","root","dbuserdbuser","SELECT * FROM e6156_project.users WHERE first_name ='"+id+"'" ), id);
+
+    @RequestMapping("/testuser/{someID}")
+    public @ResponseBody String getID(@PathVariable(value="someID") String id, String someAttr) {
+
+        return String.format(mysql_query("jdbc:mysql://users-e6156.cexqeqvqreq2.us-east-1.rds.amazonaws.com:3306/UserData?autoReconnect=true&useSSL=false","root","dbuserdbuser","SELECT * FROM UserData.users WHERE first_name ='"+id+"'" ), id);
     }
+
+
 
 
 
     public String mysql_query(String  DB_URL, String USER, String PASS,String QUERY) {
 
-        //String DB_URL = "jdbc:mysql://localhost:3306/e6156_project";
-        //String USER = "root";
-        //String PASS = "dbuserdbuser";
-        //String QUERY = "SELECT * FROM  e6156_project.users";
         System.out.println(QUERY);
-
+        String finalresult  ="";
         String result = null;
         try (
                 Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -49,20 +49,26 @@ public class DemoApplication {
                 ResultSet rs = stmt.executeQuery(QUERY);
 
 
-                ) {
+        ) {
             // Extract data from result set
+
             while (rs.next()) {
                 // Retrieve by column name
 
                 result = ("email: " + rs.getString("email"));
                 result = result + " " + ("User_Name: " + rs.getString("User_Name"));
+                result = result + " " + ("First_Name: " + rs.getString("first_name"));
+                result = result + " " + ("Last_Name: " + rs.getString("last_name"));
                 System.out.println(result);
+                finalresult=finalresult+result;
+
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return result;
+        return finalresult;
     }
+
     }
 
 
